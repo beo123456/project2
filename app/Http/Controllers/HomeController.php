42e577a8daad147e6d56819d153aa4112c57b3fd
HoangDung <77218200+beo123456@users.noreply.github.com>
2021-07-13 09:14:12 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,8 +9,8 @@ class HomeController extends Controller
 {
     function Index(){
         $banner = Banner::orderBy('id','desc')->where('status',1)->take(4)->get();
-        $category = Category::all();
-        $brand = Brand::all();
+        $category = Category::where('status',1)->get();
+        $brand = Brand::where('status',1)->get(); 
         $product = Product::where('status',1)->limit(6)->get(); /// lấy sp có stt = 1(hiển thị), giới hạn 4sp hthi
         return view('pages.home_frontend',compact('category','brand','product','banner'));
     }
@@ -18,8 +18,8 @@ class HomeController extends Controller
         // lấy keyword ng dùng muốn tìm
         $keyword = $request->keyword;
         $banner = Banner::orderBy('id','desc')->where('status',1)->take(4)->get();
-        $category = Category::all();
-        $brand = Brand::all(); 
+        $category = Category::where('status',1)->get();
+        $brand = Brand::where('status',1)->get(); 
         // lấy sp vs ddkien, keyword = name spham
         $search_product = Product::where('name','like','%'.$keyword.'%')->where('status',1)->get();
         return view('pages.product.search',compact('category','brand','search_product','banner'));
